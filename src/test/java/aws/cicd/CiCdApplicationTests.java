45034bf3f00a47b5a2adf9415432f11fd3c69142
Junsoo Choi <choix712@umn.edu>
2024-04-21 14:48:14 +0900

    Initial commit

---
+++ src/test/java/aws/cicd/CiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package aws.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
